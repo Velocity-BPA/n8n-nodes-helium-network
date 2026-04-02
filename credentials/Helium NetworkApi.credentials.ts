5ec474671a0967838d5cefa0f1173a30c5617b45
John Drefahl <john@velobpa.com>
2026-04-02 11:13:34 -0500

    chore: update credentials/Helium NetworkApi.credentials.ts to v1.0.0

--- credentials/Helium NetworkApi.credentials.ts
+++ credentials/Helium NetworkApi.credentials.ts
@@ -6,24 +6,14 @@ import {
 export class HeliumNetworkApi implements ICredentialType {
 	name = 'heliumNetworkApi';
 	displayName = 'Helium Network API';
-	documentationUrl = 'https://docs.helium.com/api/';
+	documentationUrl = 'https://docs.helium.com/api/blockchain/introduction';
 	properties: INodeProperties[] = [
 		{
-			displayName: 'API Key',
-			name: 'apiKey',
-			type: 'string',
-			typeOptions: { password: true },
-			default: '',
-			description: 'API key for Helium Network Console',
-			required: false,
-		},
-		{
 			displayName: 'API Base URL',
-			name: 'baseUrl',
+			name: 'apiUrl',
 			type: 'string',
 			default: 'https://api.helium.io/v1',
-			description: 'Base URL for Helium Network API',
-			required: true,
+			description: 'Base URL for the Helium Network API',
 		},
 	];
 }
